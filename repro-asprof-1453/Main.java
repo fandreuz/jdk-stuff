7d3b57846e3f2a446ab8394dc788e753d4c47db7
Francesco Andreuzzi <andreuzzi.francesco@gmail.com>
2025-10-05 14:37:40 +0000

    nn

--- repro-asprof-1453/Main.java
+++ repro-asprof-1453/Main.java
@@ -1,9 +1,6 @@
 import java.io.*;
 import jdk.internal.misc.Unsafe;
 import java.lang.reflect.Field;
-import java.lang.management.ManagementFactory;
-import javax.management.MBeanServer;
-import javax.management.ObjectName;
 
 public class Main {
 
@@ -18,23 +15,10 @@ public class Main {
         f.setAccessible(true);
         Unsafe unsafe = (Unsafe) f.get(null);
 
-        for (int i = 0; i < 1000; ++i) {
+        for (int i = 0; i < 10000000; ++i) {
             hookUnhook();
             unsafe.park(false, 1L);
         }
-
-        countUnsafeParkInCodeCache();
-    }
-
-    private static void countUnsafeParkInCodeCache() throws Exception {
-        MBeanServer server = ManagementFactory.getPlatformMBeanServer();
-        ObjectName on = new ObjectName("com.sun.management:type=DiagnosticCommand");
-        String op = "compilerCodelist"; // maps from jcmd "Compiler.codelist"
-        String[] args = new String[0];
-        String[] sig = new String[] { "[Ljava.lang.String;" };
-        String output = (String) server.invoke(on, op, new Object[] { args }, sig);
-        for (String s : output.split("\n")) {
-            if (s.contains("Unsafe.park")) System.err.println(s);
-        }
+	System.in.read();
     }
 }
